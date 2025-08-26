0cc1d07a4d9510e90bea1246ecce3a874ea37164
Gurnav224 <chaudharyg856@gmail.com>
2025-08-26 14:33:01 +0530

    feat: Implmented the edit , delete and date formatting

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -19,3 +19,49 @@ export const createTodo = async (req, res) => {
 };
 
 
+
+export const getTodoEditPage = async  (req, res) => {
+    try {
+        const { id } = req.params;
+        const todo = await Todo.findById(id);
+        if (!todo) {
+            return res.status(404).json({ message: "Todo not found" });
+        }
+        res.render('edit', { todo });
+    } catch (error) {
+        console.error("Error fetching todo for edit:", error);
+        res.status(500).json({ message: "Error fetching todo for edit", error });
+    }
+}
+
+export const updateTodo = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, description } = req.body;
+
+        const todo = await Todo.findByIdAndUpdate(id, { title, description }, { new: true });
+        if (!todo) {
+            return res.status(404).json({ message: "Todo not found" });
+        }
+        res.redirect('/');
+    } catch (error) {
+        console.error("Error updating todo:", error);
+        res.status(500).json({ message: "Error updating todo", error });
+    }
+};
+
+
+
+export const deleteTodo = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const todo = await Todo.findByIdAndDelete(id);
+        if (!todo) {
+            return res.status(404).json({ message: "Todo not found" });
+        }
+        res.redirect('/');
+    } catch (error) {
+        console.error("Error deleting todo:", error);
+        res.status(500).json({ message: "Error deleting todo", error });
+    }
+};
--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -1,7 +1,10 @@
 import express from 'express';
-import { createTodo  } from '../controllers/todo.controller.js';
+import { createTodo , updateTodo , getTodoEditPage , deleteTodo} from '../controllers/todo.controller.js';
 const router = express.Router();
 
 router.post('/todos', createTodo )
+router.get('/todos/:id/edit', getTodoEditPage)
+router.post('/todos/:id', updateTodo )
+router.post('/todos/:id/delete', deleteTodo)
 
 export default router;
